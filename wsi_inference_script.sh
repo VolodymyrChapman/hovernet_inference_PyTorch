027ccced22f3e55dcfee683edd3428b27fc7f37b
VolodymyrChapman <volodymyrchapman@outlook.com>
2022-05-17 15:55:28 +0100

    testing_wsi_infer

--- wsi_inference_script.sh
+++ wsi_inference_script.sh
@@ -3,11 +3,11 @@ python3 'run_infer.py' \
 --gpu=0 \
 --nr_types=5 \
 --type_info_path='type_info_inclusive_colors.json' \
---batch_size=5 \
+--batch_size=19 \
 --model_mode=fast \
 --model_path='models/checkpoints/lr_10e_-4_net_epoch=50.tar' \
---nr_inference_workers=11 \
---nr_post_proc_workers=11 \
+--nr_inference_workers=16 \
+--nr_post_proc_workers=16 \
 wsi \
 --input_dir='testing/wsi' \
 --output_dir='testing/outdata' \
