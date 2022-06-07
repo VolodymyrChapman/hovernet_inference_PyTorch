327d2bd1525186263504e9fcebad844575568f10
VolodymyrChapman <volodymyrchapman@outlook.com>
2022-06-07 17:31:21 +0100

    wsi_inference_working

--- wsi_inference_script.sh
+++ wsi_inference_script.sh
@@ -6,14 +6,10 @@ python3 'run_infer.py' \
 --batch_size=19 \
 --model_mode=fast \
 --model_path='models/checkpoints/lr_10e_-4_net_epoch=50.tar' \
---nr_inference_workers=16 \
+--nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 wsi \
 --input_dir='testing/wsi' \
 --output_dir='testing/outdata' \
 --proc_mag=40 \
---cache_path='/media/volodymyr/1TB_Store_A/Lymphoma_backup/cache' \
 --input_mask_dir='testing/mask' 
-
-
- 
